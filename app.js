152dc118e6566bdf58760700911fb09d20613257
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-09 20:38:34 +0300

    Write data to file

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   // console.log(req);
   console.log(req.url, req.method, req.headers);
+  if (req.url === '/') {
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (req.url === '/message' && req.method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
