67715b90df7b113f1c57f2d0b2b9017cfc3628d8
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-12 18:33:09 +0300

    Use routes file

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const requestHandler = require("./routes");
 
-const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url, req.method, req.headers);
-  if (req.url === '/') {
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (req.url === '/message' && req.method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    })
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    })
-
-  }
-  res.write('<html>');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-  // process.exit(1);
-});
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
