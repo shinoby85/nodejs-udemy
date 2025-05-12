6542393dab2e4f8a78279e889c6f9fd46d0e2d3f
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-12 20:53:24 +0300

    Use routes file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const requestHandler = require("./routes");
+const routes = require("./routes");
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(routes);
 
 server.listen(3000);
