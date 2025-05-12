834c4527aec4707c886b49529255d558f5dd00e3
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-05-12 23:40:39 +0300

    Remove comments

--- routes.js
+++ routes.js
@@ -44,39 +44,6 @@ const requestHandler = (req, res) => {
   }
   res.write('</html>');
   res.end();
-
-  // console.log(req);
-  // console.log(req.url, req.method, req.headers);
-  // if (req.url === '/') {
-  //   res.write('<html>');
-  //   res.write('<head><title>My first page</title></head>');
-  //   res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-  //   res.write('</html>');
-  //   return res.end();
-  // }
-  // if (req.url === '/message' && req.method === 'POST') {
-  //   const body = [];
-  //   req.on('data', (chunk) => {
-  //     console.log(chunk);
-  //     body.push(chunk);
-  //   })
-  //   return req.on('end', () => {
-  //     const parsedBody = Buffer.concat(body).toString();
-  //     const message = parsedBody.split('=')[1];
-  //     fs.writeFile('message.txt', message, err => {
-  //       res.statusCode = 302;
-  //       res.setHeader('Location', '/');
-  //       return res.end();
-  //     });
-  //   })
-  //
-  // }
-  // res.write('<html>');
-  // res.write('<head><title>My first page</title></head>');
-  // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  // res.write('</html>');
-  // res.end();
-  // process.exit(1);
 }
 
 module.exports = requestHandler;
